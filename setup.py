fcd86bb83837ec2a558bef3881bd8251fcc8fcf1
AUdel <knucklessg1@gmail.com>
2022-12-23 13:00:09 -0600

    Use requirements.txt for instal_reqs in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
 from setuptools import setup
 from webarchiver.version import __version__, __author__, __credits__
 from pathlib import Path
+import os
 import re
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 readme = Path('README.md').read_text()
 version = __version__
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=PipSession())
 readme = re.sub(r"Version: [0-9]*\.[0-9]*\.[0-9][0-9]*", f"Version: {version}", readme)
-print(f"README: {readme}")
 with open("README.md", "w") as readme_file:
     readme_file.write(readme)
 description = 'Python tool that allows you to take multiple full page screenshots of web pages without ads.'
@@ -20,13 +23,13 @@ setup(
     description=description,
     long_description=f'{readme}',
     long_description_content_type='text/markdown',
-    url='https://github.com/Knuckles-Team/webarchive',
+    url='https://github.com/Knuckles-Team/webarchiver',
     author=__author__,
     author_email='knucklessg1@gmail.com',
     license='Unlicense',
     packages=['webarchiver'],
     include_package_data=True,
-    install_requires=['selenium>=4.7.2', 'Pillow>=9.3.0', 'webdriver-manager>=3.8.5', 'piexif>=1.1.3'],
+    install_requires=[str(requirement.requirement) for requirement in requirements],
     py_modules=['webarchiver'],
     data_files=[("webarchiver",  ["webarchiver/lib/uBlock-Origin_v1.27.0.crx"])],
     package_data={'webarchiver': ['webarchiver']},
@@ -37,6 +40,8 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points={'console_scripts': ['webarchiver = webarchiver.webarchiver:main']},
 )
